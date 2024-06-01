065e753b9848da3f8043bcd3e26decd2bb4a3711
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-06-01 16:25:31 +0300

    Make initial configuration

---
+++ web_dynamic/1-hbnb.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+
+Contains:
+    Misc
+    ====
+    Script to initialize a flask web app with 1 route to display
+    the places of the application
+
+    Functions
+    =========
+    hbnb - Route function for the '/0-hbnb' url
+"""
+from flask import Flask, render_template
+from models import storage
+from models.amenity import Amenity
+from models.place import Place
+from models.state import State
+from uuid import uuid4
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def renew_session(exc):
+    storage.close()
+
+
+@app.route('/0-hbnb')
+def hbnb(id="all"):
+    all_amenities = storage.all(Amenity).values()
+    all_states = storage.all(State).values()
+    all_places = storage.all(Place).values()    
+    return (render_template("0-hbnb.html", amenities=all_amenities,
+                            all_states=all_states, 
+                            all_places=all_places,
+                            cache_id=uuid4()))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
