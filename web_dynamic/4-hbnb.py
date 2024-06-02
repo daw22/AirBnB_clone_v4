76847408826fe10bb186d29e0702d4068b14b793
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-06-02 11:33:09 +0300

    Enable filtering places by amenity

---
+++ web_dynamic/4-hbnb.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+
+Contains:
+    Misc
+    ====
+    Script to initialize a flask web app with 1 route to display
+    the places of the application
+
+    Functions
+    =========
+    hbnb - Route function for the '/4-hbnb' url
+"""
+from flask import Flask, render_template
+from models import storage
+from models.amenity import Amenity
+from models.place import Place
+from models.state import State
+from uuid import uuid4
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def renew_session(exc):
+    storage.close()
+
+
+@app.route('/4-hbnb')
+def hbnb():
+    """Handles requests to /api/v1/4-hbnb"""
+    all_amenities = storage.all(Amenity).values()
+    all_states = storage.all(State).values()
+    all_places = storage.all(Place).values()    
+    return (render_template("4-hbnb.html", amenities=all_amenities,
+                            all_states=all_states, 
+                            all_places=all_places,
+                            cache_id=uuid4()))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,103 @@
+$(document).ready(() => {
+    const apiStatus = $('#api_status');
+    $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
+        if (data.status === 'OK') {
+            apiStatus.addClass('available');
+        } else {
+            apiStatus.removeClass('available');
+        }
+    });
+
+    const amenityIds = [];
+    const amenityNames = [];
+
+    const amenityH4 = $('.filtered_amenities');
+    $('input.amenity_checkbox').on('change', (event) => {
+        const amenityId = $(event.currentTarget).attr('data-id').substring(1);
+        const amenityName = $(event.currentTarget).attr('data-name').substring(1);
+
+        if ($(event.currentTarget).is(':checked')) {
+            if (! amenityIds.includes(amenityId)) {
+                amenityIds.push(amenityId);
+                amenityNames.push(amenityName);
+
+            }
+        } else {
+            if (amenityIds.includes(amenityId)) {
+                amenityIds.splice(amenityIds.indexOf(amenityId), 1);
+                amenityNames.splice(amenityNames.indexOf(amenityName), 1);
+            }
+        }
+        let amenityNamesString = amenityNames.join(', ');
+        if (amenityNamesString.length > 27) {
+            amenityNamesString = amenityNamesString.substring(0, 26) + '...';
+        } else if (amenityNamesString.length === 0) {
+            amenityNamesString = '&nbsp;';
+        }
+        amenityH4.html(amenityNamesString);
+    });
+
+    const places = $('.places');
+    $.ajax({
+        url: 'http://127.0.0.1:5001/api/v1/places_search',
+        method: 'POST',
+        data: '{}',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: (data, textStatus) => {
+            console.log(data);
+            if (textStatus === 'success') {
+                insertPlaces(data);
+            }
+        }
+        
+    });
+    $('.filter-search').on('click', () => {
+        $('ARTICLE').remove();
+        $.ajax({
+            url: 'http://127.0.0.1:5001/api/v1/places_search',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify({"amenities": amenityIds}),
+            success: (data, textStatus) => {
+                console.log(data);
+                if (textStatus === 'success') {
+                    insertPlaces(data);
+                }
+            }
+        });
+    });
+
+    const insertPlaces = (data) => {
+        data = data.sort((place1, place2) => place1.name.localeCompare(place2.name));
+        for (place of data) {
+            places.append(`<ARTICLE>
+            <DIV class="headline">
+                <DIV class="place_holder"></DIV>
+                <H2>${ place.name }</H2>
+                <DIV class="price_by_night">$${ place.price_by_night }</DIV>
+            </DIV>
+            <DIV class="information">
+                <DIV class="max_guest">
+                    <DIV class="guest_icon place_icon"><IMG src='/static/images/icon_group.png'/></DIV>
+                    <P>${ place.max_guest } Guests</P>
+                </DIV>
+                <DIV class="number_rooms">
+                    <DIV class="bed_icon place_icon"><IMG src='/static/images/icon_bed.png'/></DIV>
+                    <P>${ place.number_rooms } Bedroom</P>
+                </DIV>
+                <DIV class="number_bathrooms">
+                    <DIV class="bath_icon place_icon"><IMG src='/static/images/icon_bath.png'/></DIV>
+                    <P>${ place.number_bathrooms } Bathroom</P>
+                </DIV>
+            </DIV>
+            <DIV class="description">
+                ${ place.description }
+            </DIV>
+        </ARTICLE>`)
+        }
+    }
+});
